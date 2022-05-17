289a2ad81826734df0f0cbaf5c6d23a49a5be975
phuocleoceo <ht10082001@gmail.com>
2022-05-18 00:51:19 +0700

    UI

--- screens/QuizzScreen.js
+++ screens/QuizzScreen.js
@@ -1,12 +1,12 @@
 import { StyleSheet, View, Text } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import DashedLine from 'react-native-dashed-line';
 import useQuizzAPI from "../hooks/useQuizzAPI"
 import { useSelector } from 'react-redux';
 
 export default function QuizzScreen()
 {
-    const { isLoading } = useSelector(state => state.quizz);
-
+    const { listQuizz, isLoading, currentQuizz } = useSelector(state => state.quizz);
     const { Load_Quizz_API } = useQuizzAPI();
     const [quizzIndex, setQuizzIndex] = useState(0);
 
@@ -22,11 +22,33 @@ export default function QuizzScreen()
     return (
         <View style={styles.container}>
             {
-                isLoading
-                    ?
-                    <Text>Loading</Text>
-                    :
-                    <Text>Done</Text>
+                !isLoading &&
+                <View style={styles.quizzApp}>
+                    <View style={styles.topBar}>
+                        <Text style={styles.currentCount}>
+                            Question {quizzIndex}/
+                            <Text style={styles.quizzLength}>{listQuizz.length}</Text>
+                        </Text>
+                    </View>
+
+                    <View style={styles.dottedLine}>
+                        <DashedLine dashLength={5} dashColor="#667186" />
+                    </View>
+
+                    <View style={styles.question}>
+                        <Text style={styles.questionContent}>
+                            {currentQuizz.question}
+                        </Text>
+                    </View>
+
+                    <View style={styles.answer}>
+                        {
+                            currentQuizz.choices.map(c => (
+                                <Text>{c}</Text>
+                            ))
+                        }
+                    </View>
+                </View>
             }
         </View>
     )
@@ -35,7 +57,40 @@ export default function QuizzScreen()
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FFF',
-        justifyContent: "center"
+        backgroundColor: "#2a354b",
+        padding: 25
     },
+    quizzApp: {
+        flex: 1
+    },
+    topBar: {
+        flex: 1,
+        top: 20
+    },
+    currentCount: {
+        fontSize: 25,
+        fontWeight: "bold",
+        color: "#667186",
+    },
+    quizzLength: {
+        fontSize: 12,
+        color: "#667186"
+    },
+
+    dottedLine: {
+        flex: 1,
+        alignSelf: "center"
+    },
+
+    question: {
+        flex: 1,
+    },
+    questionContent: {
+        color: "white",
+        fontSize: 15,
+    },
+
+    answer: {
+        flex: 6
+    }
 });
