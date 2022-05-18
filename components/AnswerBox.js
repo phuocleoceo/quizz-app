97dc8a3238b7c352591b3f1cd367459bb09960cd
phuocleoceo <ht10082001@gmail.com>
2022-05-18 15:58:45 +0700

    Circle

--- components/AnswerBox.js
+++ components/AnswerBox.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from "react";
 
 export default function AnswerBox({ answer })
@@ -6,6 +6,10 @@ export default function AnswerBox({ answer })
 
     return (
         <TouchableOpacity style={styles.answerBox}>
+            {/* <View style={styles.circleChoosen1}>
+                <View style={styles.circleChoosen2}></View>
+            </View> */}
+            <View style={styles.circle}></View>
             <Text style={styles.answerContent}>{answer}</Text>
         </TouchableOpacity>
     )
@@ -18,9 +22,34 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderColor: "#264c61",
         padding: 12,
-        margin: 10
+        margin: 10,
+        flexDirection: "row"
     },
     answerContent: {
         color: "#b5aeae"
+    },
+    circle: {
+        borderWidth: 2,
+        borderColor: "#286180",
+        width: 18,
+        height: 18,
+        borderRadius: 18,
+        marginRight: 15,
+    },
+    circleChoosen1: {
+        borderWidth: 2,
+        borderColor: "#286180",
+        width: 18,
+        height: 18,
+        borderRadius: 18,
+        marginRight: 15,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    circleChoosen2: {
+        width: 10,
+        height: 10,
+        borderRadius: 10,
+        backgroundColor: "#109094",
     }
 });
